70ae733b1ebbacadc9e9cce062cd8137da7982ae
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-03-05 21:09:20 +0100

    Klar

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 
 function submitQuiz() {
-    let points = 0; // Börja med noll poäng
+    let points = 0; // Börjar med att man har 0 poäng
   
     // Samla in alla svar:
     let answers = document.querySelectorAll("input[type='radio']:checked");
   
-    // Loopa igenom svaren och lägg till deras "value" till points:
-    for (let index = 0; index < answers.length; index++) {
+    
+    for (let index = 0; index < answers.length; index++) {    // Programmet kör igenom loopen och gör om value till points
       points += parseInt(answers[index].value);
     }
   
-    // Lägg in poängen i vår "correct-answers" span:
-    document.querySelector(".correct-answers").innerHTML = points;
+    
+    document.querySelector(".right-answers").innerHTML = points;
   
-    //Göm frågorna och visa resultatet:
-    document.querySelector(".questions").classList.toggle("invisible");
-    document.querySelector(".result").classList.toggle("invisible");
+  
+    document.querySelector(".questions").classList.toggle("invisible");   
+    document.querySelector(".result").classList.toggle("invisible");      //Gömmer frågorna och visar resultatet:
   }
   
   function resetQuiz() {
-    //Göm resultatet och visa frågorna:
-    document.querySelector(".result").classList.toggle("invisible");
+  
+    document.querySelector(".result").classList.toggle("invisible");    //Gömmer resultatet och visar frågorna:
     document.querySelector(".questions").classList.toggle("invisible");
   
-    //Nollställ formuläret:
-    document.querySelector(".questions").reset();
+    
+    document.querySelector(".questions").reset();  //Nollställer quizet 
   }
   
